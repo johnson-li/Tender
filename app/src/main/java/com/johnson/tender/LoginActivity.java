c016a0d7f6180c940926f27da7a237acc60d4b47
Johnson Li <johnsonli1993@gmail.com>
2017-04-30 00:25:42 +0800

    init commit

---
+++ app/src/main/java/com/johnson/tender/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.johnson.tender;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Johnson on 2017/4/30.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
